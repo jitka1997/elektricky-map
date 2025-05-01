8b8b62e94ca3788543a1eceaca45d0a466cd62c9
Jitka Muravská <jitka.mur@gmail.com>
2025-05-01 23:27:00 +0200

    Get rid of useless console log

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,10 +6,6 @@ type Props = {
 }
 
 const Container = ({ children, className }: PropsWithChildren<Props>) => {
-  console.log(
-    'xx',
-    twMerge('mx-auto w-full max-w-screen-2xl px-4 print:px-0', className)
-  )
   return (
     <div
       className={twMerge(
